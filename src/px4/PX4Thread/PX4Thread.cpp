d86d6639d77b2c4070651435aeeee3a43a8e12e5
guanglun <272073020@qq.com>
2021-09-02 18:14:55 +0800

    update px4

--- src/px4/PX4Thread/PX4Thread.cpp
+++ src/px4/PX4Thread/PX4Thread.cpp
@@ -1,17 +1,17 @@
 #include "PX4Thread.h"
-#include <mavlink.h>
-#include <QUdpSocket>
+
 #include <sys/time.h> //clock, gettimeofday, time
 
-#define BUFFER_LENGTH 2041 // minimum buffer size that can be used with qnx (I don't know why)
-
 #define SYSTEM_ID 1
-#define COMPONENT_ID 200
+#define COMPONENT_ID 1
 
 static uint64_t microsSinceEpoch();
 
 PX4Thread::PX4Thread()
 {
+    m_pUdpServer = new QUdpSocket();
+    connect(m_pUdpServer, SIGNAL(readyRead()), this, SLOT(dataReceived()));
+    m_pUdpServer->bind(14550);
 }
 
 void PX4Thread::run()
@@ -21,53 +21,58 @@ void PX4Thread::run()
     mavlink_message_t msg;
     uint16_t len;
 
-    float x, y, z;
+    float x = 10, y = 21, z = 13;
     float roll, pitch, yaw;
 
-    QHostAddress sender;
-    quint16 senderPort;
-    QUdpSocket *m_pUdpServer = new QUdpSocket();
-    m_pUdpServer->bind(QHostAddress::LocalHost, 7755);
+    setInterval(MAVLINK_MSG_ID_VISION_POSITION_ESTIMATE, 1000000);
+    setInterval(MAVLINK_MSG_ID_ATTITUDE, 100000);
 
     for (;;)
     {
-        /*Send Heartbeat */
-        mavlink_msg_heartbeat_pack(SYSTEM_ID, COMPONENT_ID, &msg, MAV_TYPE_QUADROTOR, MAV_AUTOPILOT_PX4, MAV_MODE_GUIDED_ARMED, 0, MAV_STATE_ACTIVE);
-        len = mavlink_msg_to_send_buffer(buf, &msg);
-        bytes_sent = m_pUdpServer->writeDatagram((const char *)buf, len, QHostAddress::Broadcast, 8086);
 
         float covariance[21] = {NAN};
-        uint8_t reset_counter = 10;
-        mavlink_msg_vision_position_estimate_pack(SYSTEM_ID, COMPONENT_ID, &msg,
+        //uint8_t reset_counter = 10;
+        mavlink_msg_vision_position_estimate_pack(1, 1, &msg,
                                                   microsSinceEpoch(),
                                                   x, y, z,
                                                   roll, pitch, yaw,
-                                                  covariance, reset_counter);
+                                                  covariance, 0);
         len = mavlink_msg_to_send_buffer(buf, &msg);
-        bytes_sent = m_pUdpServer->writeDatagram((const char *)buf, len, QHostAddress::Broadcast, 8086);
+        bytes_sent = m_pUdpServer->writeDatagram((const char *)buf, len, QHostAddress("192.168.2.1"), 14550);
 
-        if (m_pUdpServer->hasPendingDatagrams())
+        QThread::msleep(100);
+    }
+}
+
+void PX4Thread::dataReceived()
+{
+    while (m_pUdpServer->hasPendingDatagrams()) //有数据报可读
+    {
+        QByteArray datagram;
+        datagram.resize(m_pUdpServer->pendingDatagramSize());
+        m_pUdpServer->readDatagram(datagram.data(), datagram.size(), &sender, &senderPort);
+
+        // QString strMes(datagram);
+        // qDebug() << strMes;
+        mavlink_message_t msg;
+        mavlink_status_t status;
+        for (int i = 0; i < datagram.size(); i++)
         {
-            QByteArray datagram;
-            datagram.resize(m_pUdpServer->pendingDatagramSize());
-            m_pUdpServer->readDatagram(datagram.data(), datagram.size(), &sender, &senderPort);
-
-            QString strMes(datagram);
-            qDebug() << strMes;
-			mavlink_message_t msg;
-			mavlink_status_t status;
-            for(int i=0;i<datagram.size();i++)
+            if (mavlink_parse_char(MAVLINK_COMM_0, datagram.data()[i], &msg, &status))
             {
-				if (mavlink_parse_char(MAVLINK_COMM_0, datagram.data()[i], &msg, &status))
-				{
-					// Packet received
-					printf("\nReceived packet: SYS: %d, COMP: %d, LEN: %d, MSG ID: %d\n", msg.sysid, msg.compid, msg.len, msg.msgid);
-				}
+                // Packet received
+                //printf("Received packet: SYS: %d, COMP: %d, LEN: %d, MSG ID: %d\r\n", msg.sysid, msg.compid, msg.len, msg.msgid);
+                //printf(".");
+                switch (msg.msgid)
+                {
+                case MAVLINK_MSG_ID_ODOMETRY:
+                    printf("Received packet: SYS: %d, COMP: %d, LEN: %d, MSG ID: %d\r\n", msg.sysid, msg.compid, msg.len, msg.msgid);
+                    break;
+                default:
+                    break;
+                }
             }
-
         }
-
-        QThread::msleep(1000);
     }
 }
 
@@ -83,3 +88,40 @@ static uint64_t microsSinceEpoch()
 
     return micros;
 }
+
+void PX4Thread::setInterval(int id, int interval_us)
+{
+    mavlink_command_long_t interval;
+
+    interval.param1 = id;
+    interval.param2 = interval_us;
+    interval.command = MAV_CMD_SET_MESSAGE_INTERVAL;
+    interval.target_system = 1;
+    interval.target_component = 1;
+
+    mavlink_msg_command_long_encode(255, 0, &msg, &interval);
+
+    len = mavlink_msg_to_send_buffer(buf, &msg);
+    bytes_sent = m_pUdpServer->writeDatagram((const char *)buf, len, QHostAddress("192.168.2.1"), 14550);
+}
+
+/* Ctrl Success
+        mavlink_command_long_t interval;
+
+        interval.param1 = MAVLINK_MSG_ID_ATTITUDE;
+        interval.param2 = 1000000;
+        interval.command = MAV_CMD_SET_MESSAGE_INTERVAL;
+        interval.target_system = 1;
+        interval.target_component = 1;
+
+        mavlink_msg_command_long_encode(255, 0, &msg, &interval);
+
+        len = mavlink_msg_to_send_buffer(buf, &msg);
+        bytes_sent = m_pUdpServer->writeDatagram((const char *)buf, len, QHostAddress("192.168.2.1"), 14550);
+
+        //Send Heartbeat
+        // mavlink_msg_heartbeat_pack(SYSTEM_ID, COMPONENT_ID, &msg, MAV_TYPE_QUADROTOR, MAV_AUTOPILOT_PX4, MAV_MODE_GUIDED_ARMED, 0, MAV_STATE_ACTIVE);
+        // len = mavlink_msg_to_send_buffer(buf, &msg);
+        // bytes_sent = m_pUdpServer->writeDatagram((const char *)buf, len, QHostAddress("192.168.2.1"), 14551);
+
+*/
--- src/px4/PX4Thread/PX4Thread.h
+++ src/px4/PX4Thread/PX4Thread.h
@@ -3,19 +3,32 @@
 
 #include <QThread>
 #include "workspace.h"
+#include <QUdpSocket>
+#include <mavlink.h>
+
+#define BUFFER_LENGTH 2041 // minimum buffer size that can be used with qnx (I don't know why)
 
 class PX4Thread : public QThread
 {
     Q_OBJECT
 private:
+    QUdpSocket *m_pUdpServer;
+    QHostAddress sender;
+    quint16 senderPort;
+
+    uint8_t buf[BUFFER_LENGTH];
+    int bytes_sent;
+    mavlink_message_t msg;
+    uint16_t len;
 
 public:
     PX4Thread();
     void run();
+    void setInterval(int id,int interval_us);
 signals:
 
 private slots:
- 
+    void dataReceived();
 };
 
 #endif
